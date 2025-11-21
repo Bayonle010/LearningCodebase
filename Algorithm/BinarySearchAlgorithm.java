075677697303d8163b3598e75c987bcf686ccc51
Bayonle010 <afeezonadiran@gmail.com>
2025-11-21 09:43:45 +0100

    unsortedOrderBinarySearch method

---
+++ Algorithm/BinarySearchAlgorithm.java
@@ -0,0 +1,54 @@
+package Algorithm;
+
+public class BinarySearchAlgorithm  {
+    public static void main(String[] args) {
+        int [] input = {-18,2,4,6,8,9};
+        System.out.println(
+                binarySearchForAscSortedArray(input, 8)
+        );
+
+    }
+
+    private static int binarySearchForAscSortedArray(int[] array, int target){
+        int start = 0;
+        int end = array.length -1;
+        while (start<= end){
+            int mid = start + (end -start)/2;
+            if (target < array[mid]){
+                end = mid -1;
+            }else if (target > array[mid]){
+                start = mid +1;
+            }else {
+                // target found
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+
+    private static int unknownSortedOrderBinarySearch(int[] array, int target){
+        int start = 0;
+        int end = array.length -1;
+        boolean isAscOrder = (array[start] < array[end]);
+
+        while (start<= end){
+            int mid = start + (end - start)/2;
+            if (array[mid] == target) {return  mid;}
+            if (isAscOrder){
+                if (target < mid){
+                    end = mid -1;
+                }else{
+                    start = mid + 1;
+                }
+            }else{
+                if (target > array[mid]){
+                    end = mid - 1;
+                }else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
