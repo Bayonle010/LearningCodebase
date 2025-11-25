729e2edb52488c8d26540cfa9b20f86a8b692a79
Bayonle010 <afeezonadiran@gmail.com>
2025-11-25 05:25:08 +0100

    biary search with recursion

--- Algorithm/BinarySearchAlgorithm.java
+++ Algorithm/BinarySearchAlgorithm.java
@@ -51,4 +51,22 @@ public class BinarySearchAlgorithm  {
         }
         return -1;
     }
+
+
+    private static int binarySearchAlgorithmWithRecursion(int[] array, int target, int start, int end){
+        if (start > end){
+            return -1;
+        }
+
+        int m = start + (end - start)/2;
+        if (array[m] == target){
+            return m;
+        }
+
+        if (target < array[m]){
+            return binarySearchAlgorithmWithRecursion(array, target, start, m-1);
+        }
+        return binarySearchAlgorithmWithRecursion(array, target, m+1, end);
+
+    }
 }
