5c8fbd7c72cccfc0226dd8d7925e9c88eb571289
Bayonle010 <afeezonadiran@gmail.com>
2026-03-02 04:30:56 +0100

    string concat

---
+++ Java/javabasics/Demo.java
@@ -0,0 +1,12 @@
+package Java.javabasics;
+
+import javax.swing.*;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello wold");
+        String name = "Bayo";
+        name.concat("Onadian");
+        System.out.println(name);
+    }
+}
