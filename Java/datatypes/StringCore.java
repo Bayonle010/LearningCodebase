0e583d9c8841ef4b0586900142cfd4bb1a72471e
Bayonle010 <afeezonadiran@gmail.com>
2026-03-05 10:17:07 +0100

    strings

---
+++ Java/datatypes/StringCore.java
@@ -0,0 +1,11 @@
+package Java.datatypes;
+
+public class StringCore {
+    public static void main(String[] args) {
+        String name = "Bayonle";
+        System.out.println(name);
+        name = "Onadiran";
+
+        System.out.println(name);
+    }
+}
