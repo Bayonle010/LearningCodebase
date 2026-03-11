2cf9467ba28eb6ee1cd9b0fff57798e5dcc3de21
Bayonle010 <afeezonadiran@gmail.com>
2026-03-11 09:19:31 +0100

    searching an infinite array using binary searcch

--- Algorithm/BinarySearchAlgorithm.java
+++ Algorithm/BinarySearchAlgorithm.java
@@ -190,5 +190,39 @@ public class BinarySearchAlgorithm  {
     }
 
 
+    // Search in an infinite array
+
+    static int ans(int[] array, int target){
+        int start = 0;
+        int end = 1;
+
+        while (target > array[end]){
+            int newStart = end + 1;
+
+            end = end + (end - start + 1) * 2;
+            start = newStart;
+        }
+
+        return binarySearch(array, target, start, end);
+    }
+
+    static int binarySearch(int[] array, int target, int start, int end){
+        while (start <= end){
+            int mid = start + (end - start)/2;
+
+            if (target < array[mid]){
+                end = mid - 1;
+            } else if (target > array[mid]) {
+                start = mid + 1;
+
+            }else{
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+
+
 
 }
