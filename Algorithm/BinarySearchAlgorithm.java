905594f3820aa96edd41f31046895507dc7d1791
Bayonle010 <afeezonadiran@gmail.com>
2026-03-13 13:48:07 +0100

    update: peak element in a peak arrray

--- Algorithm/BinarySearchAlgorithm.java
+++ Algorithm/BinarySearchAlgorithm.java
@@ -223,6 +223,27 @@ public class BinarySearchAlgorithm  {
         return -1;
     }
 
+    public int peakIndexMountainArray(int[]  arr){
+        int start = 0;
+        int end = arr.length -1;
+
+        while (start < end){
+
+            int mid = start + (end - start)/2;
+
+            if (arr[mid] < arr[mid + 1]){
+
+                start = mid + 1;
+
+            } else {
+                end = mid;
+            }
+        }
+
+        return start; // or end
+
+    }
+
 
 
 }
