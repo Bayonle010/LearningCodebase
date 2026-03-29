95c5995c8976deb84433dfc39a52ef4d182343fe
Bayonle010 <afeezonadiran@gmail.com>
2026-03-29 08:21:11 +0100

    update: changes search in sorted array to match text cases

--- Algorithm/binarySearch/SearchInRotatedArray.java
+++ Algorithm/binarySearch/SearchInRotatedArray.java
@@ -14,16 +14,17 @@ public class SearchInRotatedArray {
 
     static int searchInRotatedArray(int[] array, int target){
         int peakIndex = peakIndex(array);
-        if (peakIndex == target){
+        if (array[peakIndex] == target){
             return peakIndex;
         }
 
         int leftSearch = binarySearchInAscOrder(array, target, 0, peakIndex - 1);
         if (leftSearch != -1){
             return  leftSearch;
-        }else {
-            return binarySearchInAscOrder(array, target, peakIndex + 1, array.length -1 );
         }
+
+        return binarySearchInAscOrder(array, target, peakIndex + 1, array.length -1 );
+
     }
 
     private  static int peakIndex(int[] array){
@@ -31,32 +32,41 @@ public class SearchInRotatedArray {
         int end = array.length -1;
 
         while (start < end){
-            int mid = start + (end - start)/2;
+            int mid = start + (end - start) / 2;
 
-            if (array[mid] < array[mid +1]){
+            if (array[mid] > array[mid + 1]) {
+                return mid; // found pivot directly
+            }
+
+            if (array[mid] >= array[start]) {
                 start = mid + 1;
-            }else if (array[mid] > array[mid + 1]){
+            } else {
                 end = mid;
-            }else {
-                return mid;
             }
         }
 
-        return start;
+        return array.length -1;
     }
 
     static int binarySearchInAscOrder(int[] array, int target, int start, int end){
-        while (start < end){
+        while (start <= end){
             int mid = start + (end - start)/2;
           if (array[mid] < target){
-                start = mid +1;
+                start = mid + 1;
           }else if (array[mid] > target){
-                end = mid;
+                end = mid -1;
           }else{
               return mid;
           }
         }
 
         return  -1;
+    }
+
+
+    //
+    static int searchInRotatedArraySecondMethod(int[] nums, int target){
+
+        return -1;
     }
 }
