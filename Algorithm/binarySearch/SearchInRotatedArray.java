7594e4549cb27b75951d8d6d2c1b04e08d5f0c3d
Bayonle010 <afeezonadiran@gmail.com>
2026-03-29 09:40:13 +0100

    update: search in rotated array method II

--- Algorithm/binarySearch/SearchInRotatedArray.java
+++ Algorithm/binarySearch/SearchInRotatedArray.java
@@ -4,7 +4,7 @@ public class SearchInRotatedArray {
     public static void main(String[] args) {
         int [] rotatedArray = {4, 5, 6, 7, 0, 1 ,2};
         int [] rotatedArray2 = {1};
-        System.out.println(searchInRotatedArray(rotatedArray, 0));
+        System.out.println(searchInRotatedArrayII(rotatedArray, 0));
         System.out.println(searchInRotatedArray(rotatedArray, 3));
         System.out.println(searchInRotatedArray(rotatedArray2, 0));
 
@@ -64,9 +64,37 @@ public class SearchInRotatedArray {
     }
 
 
-    //
-    static int searchInRotatedArraySecondMethod(int[] nums, int target){
+    // nums = [4,5,6,7,0,1,2], target = 3. output =3 and expected = -1
+    // nums = [1], target = 0, output = 0 and expected = -1
+    // [4,5,6,7,0,1,2], target = 0, output = 4, expected = 4
+    static int searchInRotatedArrayII(int[] nums, int target){
 
+        int start = 0;
+        int end = nums.length -1;
+
+        while (start <= end){
+            int mid = start + (end - start)/2;
+
+            if (nums[mid] == target){
+                return mid;
+            }
+
+            // left half is sorted
+            if (nums[start] <= nums[mid]){
+                if (target >= nums[start] && target<= nums[mid]){
+                    end = mid -1;
+                }else {
+                    start = mid + 1;
+                }
+            }else {
+                // right hand is sorted
+                if(target > nums[mid] && target <= nums[end]){
+                    start = mid + 1;
+                }else {
+                    end = mid -1;
+                }
+            }
+        }
         return -1;
     }
 }
