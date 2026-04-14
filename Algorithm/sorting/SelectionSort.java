e23982231176bc4e6b846d798aa0272be34060dd
Bayonle010 <afeezonadiran@gmail.com>
2026-04-14 05:36:08 +0100

    rename to selection sort

--- Algorithm/sorting/SelectionSort.java
+++ Algorithm/sorting/SelectionSort.java
@@ -2,7 +2,7 @@ package Algorithm.sorting;
 
 import java.util.Arrays;
 
-public class InsertionSort {
+public class SelectionSort {
 
     public static void main(String[] args) {
         int[] array = {5,6,7,4,3,2,1};
