a003d35954b3371a60ac2dd84ea36b2006a176ab
Bayonle010 <afeezonadiran@gmail.com>
2026-05-01 16:25:35 +0100

    doubly linked list

--- Java/JavaCollections/list/Main.java
+++ Java/JavaCollections/list/Main.java
@@ -2,23 +2,33 @@ package Java.JavaCollections.list;
 
 public class Main {
     public static void main(String[] args) {
-        CustomLinkedList list = new CustomLinkedList();
-        list.insertFirst(3);
-        list.insertFirst(7);
-        list.insertFirst(8);
-        list.insertFirst(10);
-        list.insertFirst(12);
-        list.insertLast(99);
-        list.insertFirst(15);
-        list.display();
-        System.out.println(list.deleteFirst());
-        list.display();
-        System.out.println(list.deleteLast());
-        list.display();
-        list.delete(2);
-        list.display();
+//        CustomLinkedList list = new CustomLinkedList();
+//        list.insertFirst(3);
+//        list.insertFirst(7);
+//        list.insertFirst(8);
+//        list.insertFirst(10);
+//        list.insertFirst(12);
+//        list.insertLast(99);
+//        list.insertFirst(15);
+//        list.display();
+//        System.out.println(list.deleteFirst());
+//        list.display();
+//        System.out.println(list.deleteLast());
+//        list.display();
+//        list.delete(2);
+//        list.display();
+//
+//        list.get(0);
 
-        list.get(0);
+        DoublyLinkedList list = new DoublyLinkedList();
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(8);
+        list.insertFirst(17);
+        list.insertLast(20);
+        list.insert(2,5);
+
+        list.display();
 
 
     }
