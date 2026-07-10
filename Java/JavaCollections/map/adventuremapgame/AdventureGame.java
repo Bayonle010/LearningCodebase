419ee8cb45afb3a1200b8317906cb1c9ecc18867
Bayonle010 <afeezonadiran@gmail.com>
2026-07-10 15:07:42 +0100

    update: completed adventure game

--- Java/JavaCollections/map/adventuremapgame/AdventureGame.java
+++ Java/JavaCollections/map/adventuremapgame/AdventureGame.java
@@ -36,9 +36,17 @@ public class AdventureGame {
 
     private Map<String, Location> adventureMap = new HashMap<>();
 
-    public AdventureGame(){}
+    public AdventureGame(){
+        this(null);
+    }
 
-    public AdventureGame(String customLocations){}
+    public AdventureGame(String customLocations){
+        loadLocations(GAME_LOCATION);
+
+        if (customLocations != null){
+            loadLocations(customLocations);
+        }
+    }
 
     private void loadLocations(String data){
         for (String s : data.split("\\R")){
@@ -47,9 +55,10 @@ public class AdventureGame {
 
             Map<Compass, String> nextPlaces = loadDirections(parts[2]);
             Location location = new Location(parts[1], nextPlaces);
+            adventureMap.put(parts[0], location);
 
         }
-        adventureMap.forEach((k,v)-> System.out.printf("%s:%s%n", k, v));
+        //adventureMap.forEach((k,v)-> System.out.printf("%s:%s%n", k, v));
     }
 
     private Map<Compass, String>  loadDirections(String nextPlaces){
@@ -68,6 +77,42 @@ public class AdventureGame {
 
         return directions;
 
+    }
+
+
+    private void visit(Location location){
+        System.out.printf("*** You're standing %s *** %n", location.description);
+        System.out.println("\tFrom here, you can see: ");
+
+        location.nextPlaces.forEach((k,v)-> {
+            System.out.printf("\t. A %s to the %s (%S) %n", v, k.getString(), k);
+        });
+
+        System.out.print("Select Your Compass (Q to quit) >>");
+    }
+
+    public void move(String direction){
+        var nextPlaces = adventureMap.get(lastPlace).nextPlaces;
+        String nextPlace = null;
+
+        if ("ENSW".contains(direction)){
+            nextPlace = nextPlaces.get(Compass.valueOf(direction));
+            if (nextPlace != null){
+                play(nextPlace);
+            }else {
+                System.out.println("!! Invalid direction, try again !!");
+            }
+        }
+    }
+
+    public void play(String location){
+        if (adventureMap.containsKey(location)){
+            Location next = adventureMap.get(location);
+            lastPlace = location;
+            visit(next);
+        }else {
+            System.out.println(location + "is an invalid location");
+        }
     }
 
 
