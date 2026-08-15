e1522c559b028293b416f425479b591bfa470493
Bayonle010 <afeezonadiran@gmail.com>
2026-08-15 18:24:38 +0100

    update: methods of creating threads

---
+++ Java/concurrency/CustomThread.java
@@ -0,0 +1,18 @@
+package Java.concurrency;
+
+public class CustomThread extends Thread {
+
+    @Override
+    public void run(){
+        for (int i = 1; i< 5; i++){
+            System.out.println(" 1 ");
+
+            try {
+                Thread.sleep(500);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
--- Java/concurrency/Main.java
+++ Java/concurrency/Main.java
@@ -1,5 +1,8 @@
 package Java.concurrency;
 
+import java.sql.Time;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
 //    public static void main(String[] args) {
 //        System.out.println(
@@ -16,18 +19,66 @@ public class Main {
 //
 //        worker.start();
 //    }
-    public static void main(String[] args) {
+//    public static void main(String[] args) {
+//
+//        Thread worker = new Thread(() -> {
+//            for (int i = 0; i < 5; i++) {
+//                System.out.println("Worker: " + i);
+//            }
+//        });
+//
+//        worker.start();
+//
+//        for (int i = 0; i < 5; i++) {
+//            System.out.println("Main: " + i);
+//        }
+//    }
 
-        Thread worker = new Thread(() -> {
-            for (int i = 0; i < 5; i++) {
-                System.out.println("Worker: " + i);
+            public static void main(String[] args) {
+                Thread currentThread = new Thread();
+                currentThread.setName("Main Guy");
+                currentThread.setPriority(Thread.MAX_PRIORITY);
+                printThreadState(currentThread);
+
+                CustomThread customThread = new CustomThread();
+                customThread.start();
+
+                Runnable myRunnable = ()->{
+                    for (int i =1; i<8 ; i ++){
+                        System.out.println("2");
+
+                        try {
+                            TimeUnit.MILLISECONDS.sleep(250);
+                        }catch (InterruptedException e){
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                Thread myThread = new Thread(myRunnable);
+                myThread.start();
+
+                for (int i = 1; i<=3; i++){
+                    System.out.println(" 0 ");
+
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                    }catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+
+
             }
-        });
 
-        worker.start();
-
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Main: " + i);
-        }
+    public static void printThreadState(Thread thread){
+        System.out.println("------------------------");
+        System.out.println("Thread ID: " + thread.getId());
+        System.out.println("Thread Name: " + thread.getName());
+        System.out.println("Thread Priority: " + thread.getPriority());
+        System.out.println("Thread State: " + thread.getState());
+        System.out.println("Thread Group: " + thread.getThreadGroup());
+        System.out.println("Thread Is Alive: " + thread.isAlive());
+        System.out.println("------------------------------");
     }
 }
