a02317214d7c9a7a87723293852f7afa6320ffbe
Bayonle010 <afeezonadiran@gmail.com>
2026-08-25 13:01:01 +0100

    update: NQueens problem with backtracking

---
+++ Algorithm/backtracking/NQueens.java
@@ -0,0 +1,86 @@
+package Algorithm.backtracking;
+
+public class NQueens {
+
+    // Time complexity  T(N) = n * T(n -1 )  + O(n^2)
+    public static void main(String[] args) {
+
+        int n = 4;
+        boolean[][] board = new boolean[n][n];
+
+        System.out.println("Number of solutions: " + queens(board, 0));
+    }
+
+    static int queens(boolean[][] board, int row) {
+
+        if (row == board.length) {
+            display(board);
+            System.out.println();
+            return 1;
+        }
+
+        int count = 0;
+
+        for (int col = 0; col < board.length; col++) {
+
+            if (isSafe(board, row, col)) {
+
+                board[row][col] = true;
+
+                count += queens(board, row + 1);
+
+                // backtrack
+                board[row][col] = false;
+            }
+        }
+
+        return count;
+    }
+
+    private static boolean isSafe(boolean[][] board, int row, int col) {
+
+        // Check same column
+        for (int i = 0; i < row; i++) {
+            if (board[i][col]) {
+                return false;
+            }
+        }
+
+        // Check upper-left diagonal
+        int maxLeft = Math.min(row, col);
+
+        for (int i = 1; i <= maxLeft; i++) {
+            if (board[row - i][col - i]) {
+                return false;
+            }
+        }
+
+        // Check upper-right diagonal
+        int maxRight = Math.min(row, board.length - col - 1);
+
+        for (int i = 1; i <= maxRight; i++) {
+            if (board[row - i][col + i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static void display(boolean[][] board) {
+
+        for (boolean[] row : board) {
+
+            for (boolean element : row) {
+
+                if (element) {
+                    System.out.print("Q ");
+                } else {
+                    System.out.print("X ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
